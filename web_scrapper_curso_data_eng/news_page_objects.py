c4bb6b9d02938ed4fd43041232ba988d2db9e005
Fer Torres <fertorresmx@gmail.com>
2020-07-25 03:13:43 -0500

    Get article data

--- web_scrapper_curso_data_eng/news_page_objects.py
+++ web_scrapper_curso_data_eng/news_page_objects.py
@@ -41,12 +41,12 @@ class ArticlePage(NewsPage):
     def __init__(self, news_site_uid, url):
         super().__init__(news_site_uid, url)
 
-        @property
-        def body(self):
-            result = self._select(self._queries['article_body'])
-            return result[0].text if len(result) else ''
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
 
-        @property
-        def title(self):
-            result = self._select(self._queries['article_title'])
-            return result[0].text if len(result) else ''
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
