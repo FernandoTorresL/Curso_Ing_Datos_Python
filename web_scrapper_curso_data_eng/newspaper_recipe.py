787a73aa26f1ad852436fbdcc682a3f4fb9e95e6
Fer Torres <fertorresmx@gmail.com>
2020-08-23 11:12:47 -0500

    Clean data

--- web_scrapper_curso_data_eng/newspaper_recipe.py
+++ web_scrapper_curso_data_eng/newspaper_recipe.py
@@ -1,4 +1,5 @@
 import argparse
+import hashlib
 import logging
 logging.basicConfig(level=logging.INFO)
 from urllib.parse import urlparse
@@ -15,6 +16,8 @@ def main(filename):
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
     df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_new_lines_from_body(df)
 
     return df
 
@@ -46,7 +49,7 @@ def _fill_missing_titles(df):
     logger.info('Filling missing titles')
     missing_titles_mask = df['title'].isna()
 
-    print(missing_titles_mask)
+    # print(missing_titles_mask)
 
     missing_titles = (df[missing_titles_mask]['url']
         .str.extract(r'(?P<missing_titles>[^/]+)$')
@@ -55,6 +58,29 @@ def _fill_missing_titles(df):
         )
 
     df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+
+    return df
+
+def _generate_uids_for_rows(df):
+    logger.info('Generating uids for each row')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+            )
+    df['uid'] = uids
+
+    return df.set_index('uid')
+
+def _remove_new_lines_from_body(df):
+    logger.info('Removing new lines from body')
+
+    stripped_body = (df
+                    .apply(lambda row: row['body'], axis=1)
+                    .apply(lambda body: list(body))
+                    .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ''), letters)))
+                    .apply(lambda letters: ''.join(letters))
+                )
+    df['body'] = stripped_body
 
     return df
 
